1144a26392266b7d9872c564c3a0bd0c8dab8e51
JoelPineda2803 <joel.pineda@corripio.com.do>
2021-08-26 12:35:32 -0400

    APP48HR

---
+++ APP48HR/MyWorker.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyFirstWindowsService
+{
+    public class MyWorker : BackgroundService
+    {
+        private readonly ILogger<MyWorker> _logger;
+
+        public MyWorker(ILogger<MyWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation($"{nameof(ExecuteAsync)} running at: {DateTimeOffset.Now}");
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
---
+++ APP48HR/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
+using MyFirstWindowsService;
+
+namespace APP48HR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+             Host.CreateDefaultBuilder(args)
+                 .UseWindowsService()
+                 .ConfigureServices((hostContext, services) =>
+                 {
+                     services.AddHostedService<MyWorker>();
+                 });
+    }
+}
